fa56383167ba1ab8547580d84610593646e94413
bhushank <bhushank@noitavonne.com>
2023-04-18 17:44:20 +0530

    04 Basics of Routing and Routing Files in Laravel | Routing Methods

--- laravel_8_first/routes/api.php
+++ laravel_8_first/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::any('/demo', function () {
+    echo "called as http://127.0.0.1:8000/api/demo";
+});
--- laravel_8_first/routes/web.php
+++ laravel_8_first/routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/demo', function () {
+    echo "demo";
+});
+Route::get('/demo2/{name}/{id?}', function ($name,$id = null) {
+    echo $name;
+    echo $id;
+});
+Route::get('/demo3/{name}/{id?}', function ($name,$id = null) {
+    $data = compact('name','id');
+    return view('demo3')->with($data);
+});
+Route::get('/demo1', function () {
+    return view('demo');
+});
+Route::post('/post_demo', function () {
+    echo "called when csrf token is present";
+});
+Route::put('/put_demo', function () {
+    echo "called update";
+});
+Route::patch('/patch_demo', function () {
+    echo "called update";
+});
+Route::delete('/delet_demo', function () {
+    echo "called for delete";
+});
+Route::any('/any_demo', function () {
+    echo "called when for any post/get";
+});
